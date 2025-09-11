f637c217f8839124c0977ad5ce9097a6ca57d05c
tejas-git64 <tejas0586@gmail.com>
2025-09-11 18:08:59 +0530

    added highlighted text

---
+++ src/components/HighlightedText/HighlightedText.tsx
@@ -0,0 +1,28 @@
+export default function HightlightedText({
+  query,
+  value,
+}: {
+  query: string;
+  value: string;
+}) {
+  function getHighlightedLetters() {
+    if (query.trim() === "") return value;
+
+    const regex = new RegExp(`(${query})`, "gi");
+    const parts = value.split(regex);
+
+    return parts.map((part, idx) =>
+      regex.test(part) ? (
+        <span key={idx} className="bg-[#FEE3C6]">
+          {part}
+        </span>
+      ) : (
+        <span key={idx}>{part}</span>
+      )
+    );
+  }
+
+  return (
+    <b className="w-full font-semibold text-xs">{getHighlightedLetters()}</b>
+  );
+}
