c2822c1f2bce93fef68ede04d21ae656faacba91
tejas-git64 <tejas0586@gmail.com>
2025-09-12 17:07:51 +0530

    added loading states and animations

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,90 +1,107 @@
-import { startTransition, useEffect, useRef, useState } from "react";
+import { memo, startTransition, useEffect, useRef } from "react";
 import loading from "../../assets/icons8-loading.gif";
 import search from "../../assets/search-4-svgrepo-com.svg";
 
-export default function SearchBar({
-  query,
-  setQuery,
-  isExpanded,
-  setIsExpanded,
-  close,
-}: {
-  query: string;
-  isExpanded: boolean;
-  setQuery: React.Dispatch<React.SetStateAction<string>>;
-  setIsExpanded: React.Dispatch<React.SetStateAction<boolean>>;
-  close: () => void;
-}) {
-  const searchEl = useRef<HTMLInputElement>(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  function toggleLoadingState(e: React.ChangeEvent<HTMLInputElement>) {
-    startTransition(() => {
-      setQuery(String(e.target.value));
-      if (e.target.value.trim() === "") {
-        setIsLoading(false);
-      } else {
-        setIsLoading(true);
-      }
-      setIsExpanded(true);
-    });
-  }
-
-  useEffect(() => {
-    function focusSearch(e: KeyboardEvent) {
-      if (e.key.toLowerCase() === "s") {
-        if (document.activeElement !== searchEl.current) e.preventDefault();
-        searchEl.current?.focus();
-      }
+const SearchBar = memo(
+  ({
+    query,
+    setQuery,
+    isLoading,
+    setIsLoading,
+    isExpanded,
+    setIsExpanded,
+    close,
+  }: {
+    query: string;
+    isExpanded: boolean;
+    isLoading: boolean;
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+    setQuery: React.Dispatch<React.SetStateAction<string>>;
+    setIsExpanded: React.Dispatch<React.SetStateAction<boolean>>;
+    close: () => void;
+  }) => {
+    const searchEl = useRef<HTMLInputElement>(null);
+    function toggleLoadingState(e: React.ChangeEvent<HTMLInputElement>) {
+      startTransition(() => {
+        setQuery(String(e.target.value));
+        if (e.target.value.trim() === "") {
+          setTimeout(() => {
+            setIsLoading(false);
+          }, 1500);
+        } else {
+          setIsLoading(true);
+          setTimeout(() => {
+            setIsLoading(false);
+          }, 1500);
+        }
+        setIsExpanded(true);
+      });
     }
-    window.addEventListener("keydown", focusSearch);
-    return () => window.removeEventListener("keydown", focusSearch);
-  }, []);
 
-  return (
-    <div className="w-full h-16 flex items-center shrink-0 justify-around pr-2 pl-1">
-      <div className="w-full flex items-center justify-start">
-        <img
-          src={isLoading ? loading : search}
-          alt="search-icon"
-          className={`${isLoading ? "w-5 mr-2 ml-3" : "w-8 mr-1 ml-2"}`}
-        />
-        <input
-          type="search"
-          ref={searchEl}
-          value={query}
-          onFocus={(e) => (e.target.placeholder = "")}
-          onChange={toggleLoadingState}
-          tabIndex={0}
-          onBlur={(e) => (e.currentTarget.placeholder = "Searching is easier")}
-          className="w-full pb-0.5 placeholder:text-neutral-400 placeholder:tracking-normal placeholder:font-medium h-full font-medium outline-none tracking-tight pr-2"
-          placeholder="Searching is easier"
-        />
-      </div>
-      <div className="w-auto shrink-0 px-2 flex items-center justify-center">
-        {isExpanded ? (
-          <button
-            className="outline-0 text-xs font-semibold border-b-2 cursor-pointer"
-            onClick={() => {
-              close();
-              setIsLoading(false);
-            }}
-          >
-            Clear
-          </button>
-        ) : (
-          <div className="w-auto text-xs h-full text-neutral-500 flex items-center justify-center">
-            <div className="w-min border-[0.5px] mt-0.5 rounded-lg pb-0.5 border-neutral-300 flex mr-2 items-center justify-center">
-              <p className="border-neutral-200 px-2 border-b-[0.5px] pb-0.5 text-extrabold pt-0.5 rounded-md">
-                s
+    useEffect(() => {
+      function focusSearch(e: KeyboardEvent) {
+        if (e.key.toLowerCase() === "s") {
+          if (document.activeElement !== searchEl.current) e.preventDefault();
+          searchEl.current?.focus();
+        }
+      }
+      window.addEventListener("keydown", focusSearch);
+      return () => window.removeEventListener("keydown", focusSearch);
+    }, []);
+
+    return (
+      <div className="flex h-16 w-full shrink-0 items-center justify-around pr-2.5 pl-1">
+        <div className="flex w-full items-center justify-start">
+          <img
+            src={isLoading ? loading : search}
+            alt="search-icon"
+            className={`${isLoading ? "mx-3 mt-0.5 w-6" : "mx-2 mt-1 w-8"}`}
+          />
+          <input
+            type="search"
+            ref={searchEl}
+            value={query}
+            onFocus={(e) => (e.target.placeholder = "")}
+            onChange={toggleLoadingState}
+            tabIndex={0}
+            onBlur={(e) =>
+              (e.currentTarget.placeholder = "Searching is easier")
+            }
+            className={`w-full ${
+              !isExpanded ? "animate-fadein" : ""
+            } h-full pr-2 text-lg font-medium tracking-tight outline-none placeholder:font-normal placeholder:tracking-normal placeholder:text-neutral-400`}
+            placeholder="Searching is easier"
+          />
+        </div>
+        <div className="flex w-auto shrink-0 items-center justify-center px-2">
+          {isExpanded ? (
+            <button
+              className={`border-b text-xs leading-3.5 outline-0 duration-500 ${
+                isExpanded ? "animate-fadein" : "animate-fadeinout"
+              } font-medium text-black`}
+              onClick={() => {
+                close();
+                setIsLoading(false);
+              }}
+            >
+              Clear
+            </button>
+          ) : (
+            <div className="animate-fadein flex h-full w-auto items-center justify-center text-xs text-neutral-500 duration-300">
+              <div className="mt-0.5 mr-2 flex w-min items-center justify-center rounded-lg border-[0.5px] border-neutral-300 pb-0.5">
+                <p className="text-extrabold rounded-md border-b-[0.5px] border-neutral-200 px-2 py-1">
+                  s
+                </p>
+              </div>
+              <p className="text-extrabold border-neutral-600 pb-0.5">
+                quick access
               </p>
             </div>
-            <p className="border-neutral-600 pb-0.5 text-extrabold">
-              quick access
-            </p>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  },
+);
+
+export default SearchBar;
