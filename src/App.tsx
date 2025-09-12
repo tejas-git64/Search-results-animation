4ede6cd2c4d9a6dacf11cd50cc506dcb494dc128
tejas-git64 <tejas0586@gmail.com>
2025-09-12 17:08:30 +0530

    added custom cursor and info

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Card from "./Card";
+import { CustomCursor } from "./components/CustomCursor/CustomCursor";
 
 function App() {
   return (
-    <div className="w-full h-full flex items-center font-mona justify-center">
+    <div className="font-mona flex h-full w-full cursor-none items-center justify-center">
       <Card />
+      <CustomCursor />
+      <p className="absolute bottom-5 text-xs text-neutral-600">Made by Tej</p>
     </div>
   );
 }
